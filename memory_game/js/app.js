675fb94318afcf7167329a1ab485d0337e591deb
Dominic Parducci <dominic.parducci@Dominics-MacBook-Pro.local>
2019-03-19 15:24:12 -0700

    created game logic

--- memory_game/js/app.js
+++ memory_game/js/app.js
@@ -1,6 +1,17 @@
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
 
-console.log('user flipped ' + cardThree);
+
+var cards = ["Queen", "Queen", "King", "King"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+
+cardsInPlay.push(cards[0]);
+cardsInPlay.push(cards[1]);
+console.log("User Flipped " + cardOne + " and " + cardTwo);
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+} else {
+    alert("Sorry, try again");
+}
